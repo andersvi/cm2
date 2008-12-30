92da586d57ffb9c41973a794da041297888d783e
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2008-12-30 14:52:52 +0000

    cm2 branch
    git-svn-id: https://svn.code.sf.net/p/commonmusic/code/branches/cm2@1657 d60aafaf-7936-0410-ba4d-d307febf7868

---
+++ src/osc.scm
@@ -0,0 +1,12 @@
+;;; **********************************************************************
+;;; Copyright (C) 2005 Todd Ingalls, Heinrich Taube
+;;; 
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Lisp Lesser Gnu Public License.
+;;; See http://www.cliki.net/LLGPL for the terms of this agreement.
+;;; **********************************************************************
+
+;;; $Name$
+;;; $Revision$
+;;; $Date$
+
